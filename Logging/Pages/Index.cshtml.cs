96cdf7220f5ba2cf3fb4c8f0cd3ac474ea2f700b
alireza sejdei <alireza.sejdei@gmail.com>
2021-07-07 21:17:15 +0430

    Add project files.

---
+++ Logging/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logging.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            //_logger.log("You Requested The Home Page");
+            Log.Fatal("this is very bad");
+        }
+    }
+}
